fda43668b91339ad143792374b8d03831546db9b
James <James@nodeloc.com>
2024-09-15 16:44:29 +0800

    add month count.

--- extend.php
+++ extend.php
@@ -4,34 +4,35 @@ namespace Nodeloc\LeaderBoard;
 
 use Flarum\Api\Serializer\ForumSerializer;
 use Flarum\Extend;
-use Flarum\Extend\ApiController;
 use Flarum\Post\Event\Posted;
 use Flarum\Discussion\Event\Started;
 use Flarum\Api\Controller\ListUsersController;
-use Flarum\User\Filter\UserFilterer;
 use Illuminate\Database\Eloquent\Builder;
 
 return [
     (new Extend\Frontend('admin'))
-        ->js(__DIR__ . '/js/dist/admin.js'),
+        ->js(__DIR__.'/js/dist/admin.js'),
 
     (new Extend\Frontend('forum'))
-        ->js(__DIR__ . '/js/dist/forum.js')
-        ->css(__DIR__ . '/resources/less/forum.less')
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/resources/less/forum.less')
         ->route('/leaderboard', 'nodeloc_leaderboard', Content\LeaderBoard::class),
 
-    new Extend\Locales(__DIR__ . '/resources/locale'),
+    new Extend\Locales(__DIR__.'/resources/locale'),
 
     (new Extend\ApiSerializer(ForumSerializer::class))
         ->attributes(PermissionBasedForumSettings::class),
-
+    (new Extend\ApiSerializer(UserSerializer::class))
+        ->attributes(AddMonthlyCountAttributes::class),
     (new Extend\Policy())
         ->globalPolicy(Access\UserPolicy::class),
     (new Extend\Event())
         ->listen(Posted::class, [Listeners\UpdateCount::class, 'postWasPosted'])
-        ->listen(Started::class, [Listeners\UpdateCount::class, 'discussionWasStarted']),
+        ->listen(Started::class, [Listeners\UpdateCount::class, 'discussionWasStarted'])
+        ->listen(PostDeleted::class, [Listeners\UpdateCount::class, 'postWasDeleted'])
+        ->listen(DiscussionDeleted::class, [Listeners\UpdateCount::class, 'discussionWasDeleted']),
     (new Extend\View())
-        ->namespace('nodeloc.leaderboard', __DIR__ . '/resources/views'),
+        ->namespace('nodeloc.leaderboard', __DIR__.'/resources/views'),
     (new Extend\ApiController(ListUsersController::class))
         ->addSortField('money')
         ->addSortField('lotteryCount')
@@ -42,9 +43,4 @@ return [
     (new Extend\Settings())
         ->default('nodeloc-leaderboard.admin.settings.link', false)
         ->default('nodeloc-leaderboard.default-sort', 'default'),
-
-    (new Extend\Middleware('api'))
-        ->add(Middleware\BeforeMiddleware::class),
-    (new Extend\Filter(UserFilterer::class))
-        ->addFilterMutator(Filter\CheckDateFilter::class),
 ];
