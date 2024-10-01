275b233700a3e89f44b21b045ca37e143766e80c
xypp <2952795729@qq.com>
2024-10-02 00:24:09 +0800

    实现查表统计

--- extend.php
+++ extend.php
@@ -3,6 +3,7 @@
 namespace Nodeloc\LeaderBoard;
 
 use Flarum\Api\Serializer\ForumSerializer;
+use Flarum\Api\Serializer\UserSerializer;
 use Flarum\Extend;
 use Flarum\Extend\ApiController;
 use Flarum\Post\Event\Posted;
@@ -24,12 +25,13 @@ return [
 
     (new Extend\ApiSerializer(ForumSerializer::class))
         ->attributes(PermissionBasedForumSettings::class),
-
+    // (new Extend\ApiSerializer(UserSerializer::class))
+    //     ->attribute('monthlyDiscussionCount', "monthlyDiscussionCount"),
     (new Extend\Policy())
         ->globalPolicy(Access\UserPolicy::class),
-    (new Extend\Event())
-        ->listen(Posted::class, [Listeners\UpdateCount::class, 'postWasPosted'])
-        ->listen(Started::class, [Listeners\UpdateCount::class, 'discussionWasStarted']),
+    // (new Extend\Event())
+    //     ->listen(Posted::class, [Listeners\UpdateCount::class, 'postWasPosted'])
+    //     ->listen(Started::class, [Listeners\UpdateCount::class, 'discussionWasStarted']),
     (new Extend\View())
         ->namespace('nodeloc.leaderboard', __DIR__ . '/resources/views'),
     (new Extend\ApiController(ListUsersController::class))
@@ -47,4 +49,10 @@ return [
         ->add(Middleware\BeforeMiddleware::class),
     (new Extend\Filter(UserFilterer::class))
         ->addFilterMutator(Filter\CheckDateFilter::class),
+    (new Extend\ApiSerializer(UserSerializer::class))
+        ->attributes(function ($s, $model, $attrs) {
+            $attrs['monthlyCommentCount'] = $model->monthly_comment_count ?? 0;
+            $attrs['monthlyDiscussionCount'] = $model->monthly_discussion_count ?? 0;
+            return $attrs;
+        })
 ];
--- src/Filter/CheckDateFilter.php
+++ src/Filter/CheckDateFilter.php
@@ -4,6 +4,7 @@ namespace Nodeloc\LeaderBoard\Filter;
 use Flarum\Filter\FilterState;
 use Flarum\Query\QueryCriteria;
 use Xypp\LocalizeDate\Helper\CarbonZoneHelper;
+use Illuminate\Database\Capsule\Manager as DB;
 
 class CheckDateFilter
 {
@@ -15,12 +16,34 @@ class CheckDateFilter
     public function __invoke(FilterState $filter, QueryCriteria $queryCriteria)
     {
         $sort = $_REQUEST['user-directory.sort'] ?? 'username';
+        $tablePrefix = DB::getTablePrefix();
 
         if ($sort == '-lastCheckinMoney') {
             $filter->getQuery()->leftJoin('user_checkin_history', 'users.id', '=', 'user_checkin_history.user_id')
                 ->where("checkin_time", "<=", $this->carbonZoneHelper->now()->utc())
                 ->where("checkin_time", ">=", $this->carbonZoneHelper->now()->setTime(0, 0)->utc())
                 ->whereNotNull("checkin_time");
+        } else if ($sort == '-monthlyCommentCount') {
+            $filter->getQuery()->selectRaw($tablePrefix . 'users.*, COALESCE(COUNT(' . $tablePrefix . 'posts.id), 0) as monthly_comment_count')
+                ->leftJoin('posts', function ($join) use ($tablePrefix) {
+                    $join->on('users.id', '=', 'posts.user_id')
+                        ->whereBetween('posts.created_at', [
+                            $this->carbonZoneHelper->now()->setDay(1)->setTime(0, 0)->utc(),
+                            $this->carbonZoneHelper->now()->utc(),
+                        ])
+                        ->where('posts.number', '>', 1);
+                })
+                ->groupBy('users.id');
+        } else if ($sort == '-monthlyDiscussionCount') {
+            $filter->getQuery()->selectRaw($tablePrefix . 'users.*, COALESCE(COUNT(' . $tablePrefix . 'discussions.id), 0) as monthly_discussion_count')
+                ->leftJoin('discussions', function ($join) use ($tablePrefix) {
+                    $join->on('users.id', '=', 'discussions.user_id')
+                        ->whereBetween('discussions.created_at', [
+                            $this->carbonZoneHelper->now()->setDay(1)->setTime(0, 0)->utc(),
+                            $this->carbonZoneHelper->now()->utc(),
+                        ]);
+                })
+                ->groupBy('users.id');
         }
     }
 }
